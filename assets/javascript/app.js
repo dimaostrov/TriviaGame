b8d01bf16965140e9a7c5023d06040ca6f36366b
Dmitriy Ostrovskiy <dmtrostrovskiy@gmail.com>
2018-03-27 20:19:07 -0700

    Answer refreshes after 3 seconds

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,11 +9,8 @@ let game = {
   getQuestions: () => {
     questions.map(x => game.qss.push(x));
   },
-  count: () => {
-
-  }
+  count: () => {}
 };
-
 
 $("#question").html(`
     <button id='start' class=''>Start Trivia</button>`);
@@ -35,25 +32,41 @@ $("button").on("click", function() {
 });
 
 $(document).on("click", ".btn", function() {
-  //e.preventDefault();
   var right = $(this).attr("correct");
-  
-  if (right == 'true' && !game.choiceMade) {
-    $(this).removeClass('btn-info')
+  if (right == "true" && !game.choiceMade) {
+    $(this).removeClass("btn-info");
     $(this).addClass("btn-success");
+    game.correct++;
     $("#answer").html($("<p>You got it right!!!</p>"));
-    addFuntip()
-  } else if (right == 'false' && !game.choiceMade) {
+    addFuntip();
+  } else if (right == "false" && !game.choiceMade) {
     $(this).addClass("btn-danger");
+    game.wrong++;
     $("#answer").html($("<p>You got it wrong!!!</p>"));
-    addFuntip()
+    addFuntip();
   }
 });
 
-// adds funtip as well as the next button and preventes choosing multiple answers
 function addFuntip() {
-    game.choiceMade = true
-    let funTip = `<p>${game.qss[game.counter - 1].funtip}</p>
-    <button id="next">Next Question</button>`
-    $('#answer').append(funTip)
+  game.choiceMade = true;
+  let funTip = `<p>${game.qss[game.counter - 1].funtip}</p>`;
+  $("#answer").append(funTip);
+  setTimeout(nextQuestions, 3000);
+}
+
+function nextQuestions() {
+  game.counter++;
+  $("#answer").empty();
+  $("#question").html(`
+      <div>
+      <div class='question'>
+            ${game.qss[game.counter - 1].question}
+            </div>
+        <div class="choices"></div>
+        </div>`);
+  game.qss[game.counter - 1].choices.map(x => {
+    $(".choices").append(`<button class="btn btn-info m-3" correct=${x.correct}>
+          <h4>${x.answer}</h4>
+          </button>`);
+  });
 }
